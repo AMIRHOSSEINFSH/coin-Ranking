74d9e9f084fe85875ff5ce5fc7930fe3a9a4fc01
amirhossein.farkhondeh <amirhossein.farkhondeh@partsoftware.com>
2021-09-22 11:28:19 +0330

    Shimmer-bug-detected-and-Fixed

--- app/src/main/java/com/code_chabok/coinranking/feature/exchanges/ExchangesFragment.kt
+++ app/src/main/java/com/code_chabok/coinranking/feature/exchanges/ExchangesFragment.kt
@@ -1,6 +1,8 @@
 package com.code_chabok.coinranking.feature.exchanges
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -32,6 +34,49 @@ class ExchangesFragment : CoinFragment() {
         binding.tv.setOnClickListener {
             findNavController().navigate(R.id.action_exchangesFragment_to_searchFragment2)
         }
+    }
+
+    companion object{
+        val TAG = "TAGFRAGMENT"
+    }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.i(TAG, "onAttach: ")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop: ")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i(TAG, "onDestroyView: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy: ")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.i(TAG, "onDetach: ")
     }
 
 }
