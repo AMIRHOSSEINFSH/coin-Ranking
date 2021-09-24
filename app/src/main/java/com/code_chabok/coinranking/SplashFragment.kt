69b228a0e0d55e7d680b42a847cf3a6e6c0614c0
Mohammad Ghasemi <mohammad214766@gmail.com>
2021-09-24 10:47:38 +0330

    #4 layout splash done

---
+++ app/src/main/java/com/code_chabok/coinranking/SplashFragment.kt
@@ -0,0 +1,32 @@
+package com.code_chabok.coinranking
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            delay(3000)
+//            findNavController().navigate(R.id.)
+        }
+    }
+
+
+}
