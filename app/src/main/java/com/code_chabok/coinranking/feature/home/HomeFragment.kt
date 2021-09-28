a62d9a96c9b9ee575955ca56e74e219ab576287e
amirhossein.farkhondeh <amirhossein.farkhondeh@partsoftware.com>
2021-09-28 16:59:32 +0330

    spinner-fixed

--- app/src/main/java/com/code_chabok/coinranking/feature/home/HomeFragment.kt
+++ app/src/main/java/com/code_chabok/coinranking/feature/home/HomeFragment.kt
@@ -1,11 +1,19 @@
 package com.code_chabok.coinranking.feature.home
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -20,19 +28,22 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class HomeFragment : CoinFragment() {
 
-    private lateinit var binding: FragmentHomeBinding
+    private var _binding: FragmentHomeBinding? = null
+    private val bining: FragmentHomeBinding? get() = _binding
     private val viewModel: HomeViewModel by viewModels()
     private lateinit var adapter: BookMarkAdapter
-
+    private val priceList = arrayListOf("2000", "3000", "4000")
+    private val timeList = arrayListOf("1h", "24h", "7d", "1m", "1y")
+    private val marketList = arrayListOf("marketCap", "C++")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
-       rootView = binding.root as CoordinatorLayout
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        rootView = bining?.root as CoordinatorLayout
 
-        return binding.root
+        return bining!!.root
     }
 
     private var isDetail = true
@@ -41,10 +52,11 @@ class HomeFragment : CoinFragment() {
 //        return this
 //    }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setShimmerIndicator(true)
-
+        setUpSpinners()
         adapter = BookMarkAdapter {
 
             Log.i("TAG", "onViewCreated:${isDetail} ")
@@ -53,54 +65,163 @@ class HomeFragment : CoinFragment() {
             }
 
             if (isDetail) {
-                findNavController().navigate(R.id.action_homeFragment_to_cryptoDetailChildFragment,
+                findNavController().navigate(
+                    R.id.action_homeFragment_to_cryptoDetailChildFragment,
                     bundle
                 )
 //                //BookMarksFragmentDirections.action_cryptoDetailFragment_self(it)
-            }
-                else {
-                    findNavController().navigate(R.id.action_homeFragment_to_cryptoDetailFragment2, bundle)
-            }
-                }
-
-                adapter.setActivity(requireActivity())
-                adapter.apply {
-                    setActivity(requireActivity())
-                    showBubble = savedInstanceState == null
-                }
-                binding.rvHome.layoutManager =
-                    object : LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false) {
-                        override fun canScrollVertically(): Boolean {
-                            return true
-                        }
-                    }
-
-                viewModel.cryptoListLiveData.observe(viewLifecycleOwner, {
-                    adapter.submitList(it)
-                setShimmerIndicator(false)
-                    binding.rvHome.visibility = View.VISIBLE
-                })
-
-
-                binding.rvHome.addItemDecoration(
-                    DividerItemDecoration(
-                        requireContext(),
-                        (binding.rvHome.layoutManager as LinearLayoutManager).orientation
-                    )
+            } else {
+                findNavController().navigate(
+                    R.id.action_homeFragment_to_cryptoDetailFragment2,
+                    bundle
                 )
-                binding.rvHome.adapter = adapter
-
-
-            }
-
-            override fun onStop() {
-                super.onStop()
-        setShimmerIndicator(false)
-            }
-
-            override fun onDestroyView() {
-                super.onDestroyView()
-                //rootView?.removeAllViews()
-                //setShimmerIndicator(false)
             }
         }
+
+        adapter.setActivity(requireActivity())
+        adapter.apply {
+            setActivity(requireActivity())
+            showBubble = savedInstanceState == null
+        }
+        bining?.rvHome?.layoutManager =
+            object : LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false) {
+                override fun canScrollVertically(): Boolean {
+                    return true
+                }
+            }
+
+        viewModel.cryptoListLiveData.observe(viewLifecycleOwner, {
+            adapter.submitList(it)
+            setShimmerIndicator(false)
+            bining?.rvHome?.visibility = View.VISIBLE
+        })
+
+
+        bining?.rvHome?.addItemDecoration(
+            DividerItemDecoration(
+                requireContext(),
+                (bining!!.rvHome.layoutManager as LinearLayoutManager).orientation
+            )
+        )
+        bining?.rvHome?.adapter = adapter
+
+    }
+
+    fun setUpSpinners() {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            _binding?.apply {
+                priceSpinner.alpha = 0.5F
+                timeSpinner.alpha = 0.5F
+                marketCapSpinner.alpha = 0.5F
+            }
+        }
+        _binding?.priceSpinner?.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                var isUp = false
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    ((p0?.getChildAt(0)) as TextView).apply {
+                        setTextColor(ContextCompat.getColor(requireContext(), R.color.spinnerBlack))
+                        setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12F)
+                    }
+                    if (!isUp) {
+                        _binding?.ivPriceArrow?.setImageResource(R.drawable.ic_arrow_up_spinner)
+                    } else {
+                        _binding?.ivPriceArrow?.setImageResource(R.drawable.ic_arrow_up_spinner)
+                        isUp = true
+                    }
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                }
+
+            }
+        _binding?.timeSpinner?.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                var isUp = false
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    ((p0?.getChildAt(0)) as TextView).apply {
+                        setTextColor(ContextCompat.getColor(requireContext(), R.color.spinnerBlack))
+                        setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12F)
+                    }
+                    if (!isUp) {
+                        _binding?.ivTimeArrow?.setImageResource(R.drawable.ic_arrow_up_spinner)
+                    } else {
+                        _binding?.ivTimeArrow?.setImageResource(R.drawable.ic_arrow_up_spinner)
+                        isUp = true
+                    }
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                }
+
+            }
+        _binding?.marketCapSpinner?.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                var isUp = false
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    ((p0?.getChildAt(0)) as TextView).apply {
+                        setTextColor(ContextCompat.getColor(requireContext(), R.color.spinnerBlack))
+                        setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12F)
+                    }
+                    if (!isUp) {
+                        _binding?.ivMarketCapArrow?.setImageResource(R.drawable.ic_arrow_up_spinner)
+                    } else {
+                        _binding?.ivMarketCapArrow?.setImageResource(R.drawable.ic_arrow_up_spinner)
+                        isUp = true
+                    }
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                }
+
+            }
+
+        var priceListAdapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, priceList)
+        var timeListAdapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, timeList)
+        var marketCapAdapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, marketList)
+
+        _binding?.apply {
+            priceSpinner.adapter = priceListAdapter
+            timeSpinner.adapter = timeListAdapter
+            marketCapSpinner.adapter = marketCapAdapter
+        }
+
+        _binding?.priceSpinner?.setOnTouchListener { view, motionEvent ->
+
+            if (MotionEvent.ACTION_DOWN == motionEvent.action) {
+                _binding?.ivPriceArrow?.setImageResource(R.drawable.ic_arrow_down_spinner)
+            }
+            view.performClick()
+        }
+
+        _binding?.timeSpinner?.setOnTouchListener { view, motionEvent ->
+
+            if (MotionEvent.ACTION_DOWN == motionEvent.action) {
+                _binding?.ivTimeArrow?.setImageResource(R.drawable.ic_arrow_down_spinner)
+            }
+            view.performClick()
+        }
+
+        _binding?.timeSpinner?.setOnTouchListener { view, motionEvent ->
+
+            if (MotionEvent.ACTION_DOWN == motionEvent.action) {
+                _binding?.ivMarketCapArrow?.setImageResource(R.drawable.ic_arrow_down_spinner)
+            }
+            view.performClick()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        setShimmerIndicator(false)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+
+    }
+}
