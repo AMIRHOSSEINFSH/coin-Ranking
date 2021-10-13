f4e0224839bd5791e76d1bd09c9e77e5e49b1f74
amirhossein.farkhondeh <amirhossein.farkhondeh@partsoftware.com>
2021-10-13 09:48:34 +0330

    Big-merge

--- app/src/main/java/com/code_chabok/coinranking/feature/exchangeDetail/ExchangeDetailViewModel.kt
+++ app/src/main/java/com/code_chabok/coinranking/feature/exchangeDetail/ExchangeDetailViewModel.kt
@@ -3,23 +3,22 @@ package com.code_chabok.coinranking.feature.exchangeDetail
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.switchMap
 import com.code_chabok.coinranking.common.CoinViewModel
-import com.code_chabok.coinranking.domain.GetExchangeDetailUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ExchangeDetailViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    getExchangeDetailUseCase: GetExchangeDetailUseCase
+    //getExchangeDetailUseCase: GetExchangeDetailUseCase
 ) : CoinViewModel() {
 
     private var id:String = savedStateHandle.get<String>("id")
        ?: throw IllegalArgumentException("id must not be null")
 
 
-    val exchangeResource = refreshing.switchMap {
+    /*val exchangeResource = refreshing.switchMap {
         getExchangeDetailUseCase(id)
-    }
+    }*/
 
 
 }
